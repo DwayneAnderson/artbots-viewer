e525f232134e9c04d33c30fe33513ab0bb110c0c
Dwayne <dwayne.anderson+github@gmail.com>
2021-05-16 14:57:23 -0700

    chore(Server): Add fastify server; Add concurrently

---
+++ src/server/index.js
@@ -0,0 +1,29 @@
+const fastify = require('fastify')({
+  logger: true
+})
+const fs = require('fs')
+const path = require('path')
+const resolvePath = require('resolve-path')
+
+fastify.register(require('fastify-static'), {
+  root: path.join(__dirname, '..', '../build')
+})
+
+fastify.get('/api/*', async (request, reply) => {
+  return { hello: 'world', foo: ['bar', 'baz', 'quux'] }
+})
+
+fastify.get('/', async (request, reply) => {
+  const stream = fs.createReadStream(resolvePath('build/index.html'))
+  reply.type('text/html').send(stream)
+})
+
+const start = async () => {
+  try {
+    await fastify.listen(5000)
+  } catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+}
+start()
