ae29db788b07af48017854116b4bdb7f6f4f7d21
Dwayne <dwayne.anderson+github@gmail.com>
2021-05-17 00:36:17 -0700

    feature(API): rough first pass: fetch tweets from Twitter API

--- src/server/index.js
+++ src/server/index.js
@@ -1,3 +1,12 @@
+require('dotenv').config()
+const TwitterClient = require('twitter-api-client')
+const twitter = new TwitterClient.TwitterClient({
+  apiKey: process.env.TWITTER_API_KEY,
+  apiSecret: process.env.TWITTER_API_KEY_SECRET,
+  accessToken: process.env.TWITTER_API_ACCESS_TOKEN,
+  accessTokenSecret: process.env.TWITTER_API_ACCESS_TOKEN_SECRET
+})
+
 const fastify = require('fastify')({
   logger: true
 })
@@ -9,8 +18,25 @@ fastify.register(require('fastify-static'), {
   root: path.join(__dirname, '..', '../build')
 })
 
-fastify.get('/api/*', async (request, reply) => {
-  return { hello: 'world', foo: ['bar', 'baz', 'quux'] }
+fastify.get('/api/tweets', async (request, reply) => {
+  let twitterError = false
+  const tweets = []
+  const twitterRequest = await twitter.accountsAndUsers.listsStatuses({
+    list_id: process.env.TWITTER_LIST_ID,
+    count: 200
+  }).catch(error => {
+    twitterError = error
+    console.error('Twitter API error: ', error)
+  })
+  if (twitterError) {
+    return { error: twitterError }
+  }
+  twitterRequest.forEach(tweet => {
+    const { id, text, source, user } = tweet
+    const img = tweet?.entities?.media?.[0]?.media_url
+    if (img) tweets.push({ id, text, source, img, user: user.screen_name })
+  })
+  return tweets
 })
 
 fastify.get('/', async (request, reply) => {
