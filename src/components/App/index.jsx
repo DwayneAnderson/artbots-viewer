aa40217e0f4359eef0e0f27ed533c13fd394ee17
Dwayne <dwayne.anderson+github@gmail.com>
2021-05-18 07:33:58 -0700

    chore(Client): Add ErrorBoundary

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
+import { ErrorBoundary } from 'react-error-boundary'
 import Index from './../Index'
+import Error from './../UI/Error'
+
 import './style.scss'
 
 const App = () => {
   return (
-    <div className='App'>
-      <Index />
-    </div>
+    <ErrorBoundary
+      FallbackComponent={Error}
+    >
+      <div className='App'>
+        <Index />
+      </div>
+    </ErrorBoundary>
   )
 }
 
