cdb05994e85db1f247b036240fbe8c7acdd5ed17
Dwayne <dwayne.anderson+github@gmail.com>
2021-06-05 12:25:22 -0700

    chore(Client): Tweets: modifications for new backend on Netlify

--- src/components/Tweets/http.js
+++ src/components/Tweets/http.js
@@ -1,5 +1,15 @@
+const API_URL = process.env.NODE_ENV === 'development'
+  ? 'http://localhost:8888/.netlify/functions'
+  : 'https://artbots-viewer-functions.netlify.app/.netlify/functions'
+
 const fetchTweets = (listId) =>
-  fetch(`/api/tweets/${listId}`)
+  fetch(`${API_URL}/tweets?listId=${listId}`)
     .then(tweets => tweets.json())
+    .catch(error => {
+      console.error(error)
+      return {
+        error: 'Failed to load tweets!'
+      }
+    })
 
 export { fetchTweets }
