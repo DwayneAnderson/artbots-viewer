d6d4064a8dc39325bbe525cc1459811044e02300
Dwayne <dwayne.anderson+github@gmail.com>
2021-06-06 20:33:47 -0700

    chore: more artbots...

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { ErrorBoundary } from 'react-error-boundary'
 import Index from './../Index'
 import Tweets from './../Tweets'
+import All from './../All'
 import Error from './../UI/Error'
 
 import './style.scss'
@@ -20,6 +21,9 @@ const App = () => {
             </Route>
             <Route path='/list/:listId?'>
               <Tweets />
+            </Route>
+            <Route path='/all'>
+              <All />
             </Route>
           </Switch>
         </Router>
